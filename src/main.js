6190ff29f312d199dc451d4d87bddb5854563407
Hyunjae <yuhyunjae12@naver.com>
2019-11-25 20:04:14 +0900

    routing, reload 문제 수정

--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+// 로그인 토큰이 있다면 유저 정보를 가져온다.
+// 이부분도 수정이 필요..
+const token = localStorage.getItem('X-AUTH-TOKEN');
+
 new Vue({
   store,
   router,
   vuetify,
   beforeCreate(){
-    this.$store.dispatch("getMemberInfo")
+    if(token !== null){
+      this.$store.dispatch("getMemberInfo")
+    }
   },
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import store from '../store'
 
 Vue.use(VueRouter)
 
+const token = localStorage.getItem('X-AUTH-TOKEN');
+
 const rejectAuthUser =(to, from, next) =>{
   if(store.state.isLogin === true){
     alert('이미 로그인 하였습니다.')
@@ -13,7 +15,7 @@ const rejectAuthUser =(to, from, next) =>{
   }
 }
 
-const onlyAuthUser =(to, from, next) =>{
+/* const onlyAuthUser =(to, from, next) =>{
   if(store.state.isLogin === false){
     console.log(store.state.isLogin);
     alert('로그인이 필요합니다.')
@@ -22,57 +24,69 @@ const onlyAuthUser =(to, from, next) =>{
     console.log(store.state.isLogin);
     next()
   }
+} */
+
+// 페이지 리로드시 store.state.isLogin 데이터가 바인딩 되기 전에 실행 됨..
+// 로컬 스토리지에 저장된 토큰여부로 수정 하였으나 .. 
+// 수정 필요..
+const onlyAuthUser =(to, from, next) =>{
+  if(token == null){
+    alert('로그인이 필요합니다.')
+    next("/login")
+  }else{
+    next()
+  }
 }
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
     name: 'login',
     beforeEnter: rejectAuthUser,
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/signup',
     name: 'signup',
     beforeEnter: rejectAuthUser,
-    component: () => import(/* webpackChunkName: "login" */ '../views/Signup.vue')
+    component: () => import('../views/Signup.vue')
   },
   {
     path: '/mypage',
     name: 'mypage',
     beforeEnter: onlyAuthUser,
-    component: () => import(/* webpackChunkName: "mypage" */ '../views/Mypage.vue')
+    component: () => import('../views/Mypage.vue')
   },
   {
     path: '/board',
     name: 'board',
     beforeEnter: onlyAuthUser,
-    component: () => import(/* webpackChunkName: "board" */ '../views/Board.vue')
+    component: () => import('../views/Board.vue')
   },
   {
     path: '/boardDetail/:postId',
     name: 'boardDetail',
     beforeEnter: onlyAuthUser,
     props: true,
-    component: () => import(/* webpackChunkName: "board" */ '../views/BoardDetail.vue')
+    component: () => import('../views/BoardDetail.vue')
   },
   {
     path: '/boardModify/:postId',
     name: 'boardModify',
     beforeEnter: onlyAuthUser,
     props: true,
-    component: () => import(/* webpackChunkName: "board" */ '../views/BoardModify.vue')
+    component: () => import('../views/BoardModify.vue')
   },
   {
     path: '/boardInsert',
     name: 'boardInsert',
     beforeEnter: onlyAuthUser,
-    component: () => import(/* webpackChunkName: "board" */ '../views/BoardInsert.vue')
+    component: () => import('../views/BoardInsert.vue')
   },
 ]
 
