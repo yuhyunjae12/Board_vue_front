85f23bd19e1661679262eef4fd9aca2df1e6b868
Hyunjae <yuhyunjae12@naver.com>
2019-11-25 20:56:52 +0900

    마이페이지 ui 수정

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -2,74 +2,86 @@
     <div class="pa-5">
         <v-stepper v-model="e1">
             <v-stepper-items>
-            <v-stepper-content step="1">
+            <v-stepper-content
+             step="1"
+             >
                 <v-card
-                class="mb-12"
-                height="200px"
-                outlined
-                >
-                <!-- <v-card-title>
-                    UserName : {{userInfo.name}}
-                </v-card-title> -->
-                <v-card-subtitle>
-                    <v-card-text>
-                    UserName : {{userInfo.name}} <br/>
-                    UserId : {{userInfo.id}} <br/>
-                    UserRole : {{userInfo.role}}
-                    </v-card-text>
-                </v-card-subtitle>
-                </v-card>
-
-                <v-btn
-                color="primary"
-                @click="e1 = 2"
-                >
-                Modify
-                </v-btn>
-
+                    class="mx-auto"
+                    max-width="344"
+                    >
+                    <v-img
+                        src="http://digitalchosun.dizzo.com/site/data/img_dir/2015/08/27/2015082711745_0.jpg"
+                        height="180px"
+                    ></v-img>
+                    <v-card-title>
+                        {{userInfo.id}}
+                    </v-card-title>
+                    <v-card-subtitle>
+                        {{userInfo.name}}<br/>
+                        {{userInfo.role}}
+                    </v-card-subtitle>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-btn
+                        color="primary"
+                        @click="e1 = 2"
+                        >
+                        Modify
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                    </v-card>
             </v-stepper-content>
 
             <v-stepper-content step="2">
                 <v-card
-                class="mb-12"
-                height="200px"
-                outlined
-                >
+                    class="mx-auto"
+                    max-width="344"
+                    >
+                    <v-img
+                        src="https://petrico.site/wp-content/uploads/2018/09/20180928_160957.jpg"
+                        height="180px"
+                    ></v-img>
+                    <v-card-title>
+                        {{userInfo.id}}
+                    </v-card-title>
+                    <v-card-subtitle>
+                        {{userInfo.name}}<br/>
+                        {{userInfo.role}}
+                    </v-card-subtitle>
+                    <v-expand-transition>
+                        <div>
+                        <v-divider></v-divider>
+                        <v-card-text>
+                            <v-text-field
+                            v-model="password"
+                            :counter="25"
+                            :rules="passwordRules"
+                            label="password"
+                            type="password"
+                            required
+                            style="max-width:250px;"
+                            ></v-text-field>
+                        </v-card-text>
+                        </div>
+                    </v-expand-transition>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-btn
+                        color="success"
+                        @click="goModify"
+                        >
+                        Modify
+                        </v-btn>
 
-                <!-- <v-card-title>
-                    UserName : {{userInfo.name}}
-                </v-card-title> -->
-                <v-card-subtitle>
-                    <v-card-text>
-                        UserName : {{userInfo.name}} <br/>
-                        UserId : {{userInfo.id}} <br/>
-                        UserRole : {{userInfo.role}}
-                        <v-text-field
-                        v-model="password"
-                        :counter="25"
-                        :rules="passwordRules"
-                        label="password"
-                        type="password"
-                        required
-                        style="max-width:250px;"
-                        ></v-text-field>
-                    </v-card-text>
-                </v-card-subtitle>
-
-                </v-card>
-
-                <v-btn
-                color="success"
-                @click="goModify"
-                >
-                Modify
-                </v-btn>
-
-                <v-btn 
-                    text
-                    @click="e1=1"
-                >Cancel
-                </v-btn>
+                        <v-btn 
+                            text
+                            @click="e1=1"
+                        >Cancel
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                    </v-card>
             </v-stepper-content>
             </v-stepper-items>
         </v-stepper>
@@ -101,6 +113,7 @@ export default {
 
             axios.put("/v1/user/", ParamsPost).then(res =>{
                 console.log(res);
+                this.$store.dispatch('logout')
                 this.$router.push({ name: "home" });
             })
             .catch(err =>{
---
+++ src/views/MypageV1.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="pa-5">
+        <v-stepper v-model="e1">
+            <v-stepper-items>
+            <v-stepper-content step="1">
+                <v-card
+                class="mb-12"
+                height="200px"
+                outlined
+                >
+                <v-card-subtitle>
+                    <v-card-text>
+                    UserName : {{userInfo.name}} <br/>
+                    UserId : {{userInfo.id}} <br/>
+                    UserRole : {{userInfo.role}}
+                    </v-card-text>
+                </v-card-subtitle>
+                </v-card>
+
+                <v-btn
+                color="primary"
+                @click="e1 = 2"
+                >
+                Modify
+                </v-btn>
+
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+                <v-card
+                class="mb-12"
+                height="200px"
+                outlined
+                >
+                <v-card-subtitle>
+                    <v-card-text>
+                        UserName : {{userInfo.name}} <br/>
+                        UserId : {{userInfo.id}} <br/>
+                        UserRole : {{userInfo.role}}
+                        <v-text-field
+                        v-model="password"
+                        :counter="25"
+                        :rules="passwordRules"
+                        label="password"
+                        type="password"
+                        required
+                        style="max-width:250px;"
+                        ></v-text-field>
+                    </v-card-text>
+                </v-card-subtitle>
+
+                </v-card>
+
+                <v-btn
+                color="success"
+                @click="goModify"
+                >
+                Modify
+                </v-btn>
+
+                <v-btn 
+                    text
+                    @click="e1=1"
+                >Cancel
+                </v-btn>
+            </v-stepper-content>
+            </v-stepper-items>
+        </v-stepper>
+    </div>
+</template>
+<script>
+import axios from "axios"
+
+export default {
+    data(){
+        return{
+            e1: 0,
+            userInfo : this.$store.getters.getUserInfo,
+            valid: true,
+            author: '',
+            password: '',
+            passwordRules:[
+                v => !!v || 'password is required',
+            ]
+        }
+    },
+    methods:{
+        goModify(){
+            
+            let ParamsPost ={
+                msrl: this.userInfo.msrl,
+                password: this.password
+            };
+
+            axios.put("/v1/user/", ParamsPost).then(res =>{
+                console.log(res);
+                this.$router.push({ name: "home" });
+            })
+            .catch(err =>{
+            console.log(err);
+            })
+        }
+    }
+}
+</script>
